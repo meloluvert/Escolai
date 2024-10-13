3815bceb8a206dc5ad28bd95a2517d5426e7aa54
olima1 <leandro.gabriel271206@gmail.com>
2024-10-13 18:14:51 -0300

    Telas de editar e tela de anuncio

---
+++ resources/views/editarEscola.blade.php
@@ -0,0 +1,95 @@
+@extends('layout')
+<div>
+    <h3 style="display:flex; align-items: center; justify-content: center;">EDITAR</h3>
+</div>
+<div style="display:flex; gap: 20%; align-items: center; justify-content: center;">
+    <div>
+        <h4>ESCOLA</h4>
+    </div>
+</div>
+<div style="display:flex; align-items: center; justify-content: center;">
+    <form>
+        <div style="display:flex">
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">Nome*</label>
+                <input type="text" class="form-control" id="nome" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">Telefone*</label>
+                <input type="tel" class="form-control" id="numero">
+            </div>
+        </div>
+        <div style="display:flex">
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">E-mail*</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div>
+                <label for="exampleInputEmail1" class="form-label">Foto de Perfil*</label>
+                <input type="file" name="avatar" id="avatar">
+            </div>
+        </div>
+        <div class="mb-3" style="align-items: center; justify-content: center;">
+            <label for="exampleInputPassword1" class="form-label">Senha*</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3" style="align-items: center; justify-content: center;">
+            <label for="exampleInputPassword1" class="form-label">Confirmar Senha*</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3" style="align-items: center; justify-content: center;">
+            <label for="exampleInputEmail1" class="form-label">CEP*</label>
+            <input type="tel" class="form-control" id="cep">
+        </div>
+        <div style="display:flex">
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">Endereço*</label>
+                <input type="text" class="form-control" id="endereco" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">Número*</label>
+                <input type="number" class="form-control" id="numcasa">
+            </div>
+        </div>
+        <div style="display:flex">
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">Estado*</label>
+                <input type="text" class="form-control" id="estado" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">cidade*</label>
+                <input type="text" class="form-control" id="cidade">
+            </div>
+        </div>
+        <div style="display:flex">
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">Nº de Alunos*</label>
+                <input type="number" class="form-control" id="nalunos" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">Média de Mensalidade*</label>
+                <input type="number" class="form-control" id="mensalidade">
+            </div>
+        </div>
+        <div class="mb-3" style="align-items: center; justify-content: center;">
+            <label for="exampleInputEmail1" class="form-label">Nome do Coordenador*</label>
+            <input type="text" class="form-control" id="nomecoordenador">
+        </div>
+        <div class="mb-3" style="align-items: center; justify-content: center;">
+            <label for="exampleInputEmail1" class="form-label">CPF do Coordenador*</label>
+            <input type="tel" class="form-control" id="cpfcoordenador">
+        </div>
+        <div style="display:flex">
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">Instagram</label>
+                <input type="text" class="form-control" id="instagram">
+            </div>
+            <div class="mb-3" style="align-items: center; justify-content: center;">
+                <label for="exampleInputEmail1" class="form-label">Whatsapp</label>
+                <input type="tel" class="form-control" id="whatsapp">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Cancelar</button>
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </form>
+</div>
