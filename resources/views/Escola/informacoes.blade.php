302a590628333cc53cb6db593676e23335e88ad1
olima1 <leandro.gabriel271206@gmail.com>
2024-10-31 07:34:14 -0300

    Implementação do gráfico

--- resources/views/Escola/informacoes.blade.php
+++ resources/views/Escola/informacoes.blade.php
@@ -85,10 +85,7 @@
                         <span>maristavga@dominio.com</span>
 
                     </div>
-                    <div class="row">
-                        <span>Escolaridade dos Professores </span>
-                        <img src="{{ asset('storage/imgs/graficoTeste.png') }}" alt="">
-                    </div>
+                    <div id="piechart"></div>
 
                 </div>
 
@@ -152,4 +149,27 @@
             </nav>
         </div>
     </div>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Doutorado', 1],
+  ['Graduação', 5],
+  ['Pós-Graduação', 4],
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'title':'', 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
+</script>
 @endsection
